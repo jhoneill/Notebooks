2f01fb4a30d79cc7f458cf7694f340cbc37e246a
jhoneill <jhoneill@waitrose.com>
2021-12-19 15:29:38 +0000

    Add Out-Tree & Out-Mermaid to NotebookOutput.ps1 Replacement Write-Progress

--- notebooks/NotebookOutput.ps1
+++ notebooks/NotebookOutput.ps1
@@ -1,4 +1,4 @@
-Function Out-Cell{
+function Out-Cell{
     <#
         .synopsis
             Outputs a notebook cell - takes a script block, or html or objects to format as a list/table
@@ -99,7 +99,12 @@ Function Out-Cell{
         [Microsoft.DotNet.Interactive.Kernel]::HTML($html) | Out-Display -passthru:$PassThru
     }
 }
+
 function Write-Progress {
+<#
+    .SYNOPSIS
+        Notebook friendly replacement for the Write-Progress cmdlet. Similar to the "Minimal" view implemented in PowerShell 7.2
+#>
     param (
         [Parameter(Mandatory=$true,position = 0)]
         [string]$Activity,
@@ -129,6 +134,31 @@ function Write-Progress {
 }
 
 function Out-Mermaid {
+    <#
+      .DESCRIPTION
+        Accepts a mermaid chart definition as a parameter (example with the definition) or from the  pipeline
+        and outputs the minimum correct HTML / Javascript but  **depends on the kernel extension being loaded**
+
+        For examples see the mermaid home page at https://mermaid-js.github.io/mermaid/#/
+        Has an alias of `Mermaid` it can be called in a more dsl-y style);
+
+        .EXAMPLE
+        ps >Mermaid @'
+        sequenceDiagram
+            participant Alice
+            participant Bob
+            Alice->>John: Hello John, how are you?
+            loop Healthcheck
+                John->>John: Fight against hypochondria
+            end
+            Note right of John: Rational thoughts <br/>prevail!
+            John-->>Alice: Great!
+            John->>Bob: How about you?
+            Bob-->>John: Jolly good!
+        '@
+
+        Outputs a sample diagram found on the mermaid home page
+    #>
     [alias('Mermaid')]
     param (
         [parameter(ValueFromPipeline=$true,Mandatory=$true,Position=0)]
@@ -152,4 +182,93 @@ else {loadMermaid_$guid();}
 "@  }
     process {$Mermaid +=  ("`r`n" + $Text -replace '^[\r\n]+','' -replace '[\r\n]+$','') }
     end     {[Microsoft.DotNet.Interactive.Kernel]::HTML(($html -replace  '~~Mermaid~~',$mermaid ))  | Out-Display }
+}
+
+function Out-TreeView {
+<#
+      .SYNOPSIS
+        Outputs a treeview to a notebook.
+
+      .DESCRIPTION
+        C# can show JSON as a tree view; this takes an input in a similar way to Format-Table and Creates the same HTML
+
+      .PARAMETER InputObject
+        Specifies the objects to format. Enter a variable that contains the objects, or type a command or expression that gets the objects.
+
+      .PARAMETER Property
+        Specifies the properties to select. Wildcards are permitted.
+
+      .PARAMETER ExcludeProperty
+        Specifies the properties that the selection process excludes from the operation. Wildcards are permitted.
+
+      .PARAMETER Display
+        If specified the tree view will be displayed, otherwise it will will be returned as a HTML
+
+      .PARAMETER Unique
+        Specifies that if a subset of the input objects has identical properties and values, only a single member of the subset will be selected. Unique selects values after other filtering parameters are applied.
+
+      .PARAMETER Last
+        Specifies the number of objects to select from the end of an array of input objects.
+
+      .PARAMETER First
+        Specifies the number of objects to select from the beginning of an array of input objects.
+
+      .PARAMETER Skip
+        Skips (does not select) the specified number of items. By default, the Skip parameter counts from the beginning of the array or list of objects, but if the command uses the Last parameter, it counts from the end of the list or array.
+
+      .PARAMETER TreeviewCss
+        Replaces the default style sheet for formatting
+
+      .PARAMETER TitleHtml
+        The title to be included - formatted as HTML
+#>
+    param   (
+        [Parameter(Position=0, ValueFromPipeline=$true, Mandatory=$true)]
+        $InputObject,
+        [string]$TitleHtml ='Tree view',
+        [Parameter(Position=1)]
+        $Property,
+        $ExcludeProperty,
+        [switch]$Display,
+        [switch]$Unique,
+        [int32]$Last,
+        [int32]$First,
+        [int32]$Skip,
+        [string]$TreeviewCss
+    )
+    begin   {
+        if ($ExcludeProperty -and -not $Property) {$Property = '*'}
+        $rows = @()
+        if (-not $PSBoundParameters.ContainsKey('$TreeviewCss')) { $treeViewCss = @'
+<style id="dni-styles-JsonElement">
+    .dni-code-hint { font-style: italic; overflow: hidden;  white-space: nowrap;}
+    .dni-treeview  { white-space: nowrap; }
+    .dni-treeview td { vertical-align: top;}
+    details.dni-treeview {padding-left: 1em;}
+</style>
+'@ }}
+    process { $rows += $InputObject}
+    end     {
+        $selectParams = @{}
+        foreach ($param in @( 'Property', 'ExcludeProperty', 'Unique', 'Last', 'First', $Skip)){
+            if ($PSBoundParameters.ContainsKey($param)) {$selectParams[$param] = $PSBoundParameters[$param]}
+        }
+        if     ($selectParams.Count -ge 1) {$rows = $rows | Select-Object @selectParams }
+        #using psobject.Properties we get them in the correct order, not alphabetically.
+        $propNames   = $rows[0].psobject.Properties.name
+        $outputHtml  = '<details class="dni-treeview"><summary><span class="dni-code-hint">{0}</span></summary><div><table>' -f $TitleHtml
+        $outputHtml += "`r`n  <thead>`r`n    <tr>"
+        foreach ($p in $propNames) {$outputHtml += "<td>$p</td>"}
+        $outputHtml += "</tr>`r`n  </thead>`r`n  <tbody>"
+        foreach ($r in $rows) {
+            $outputHtml += "`r`n    <tr>"
+            foreach ($p in $propNames) {
+                $outputHtml += '<td><span><div class="dni-plaintext">{0}</div></span></td>' -f $r.$p
+            }
+            $outputHtml += "</tr>"
+        }
+        $outputHtml += "`r`n  </tbody>`r`n</table></div></details>`r`n"
+        if ($Display) {[Microsoft.DotNet.Interactive.Kernel]::HTML(($outputHtml + $treeViewCss))  | Out-Display}
+        else          {[Microsoft.DotNet.Interactive.Kernel]::HTML( $outputHtml)  }
+    }
 }
